efb473bab59b18195ec5ef83059a46de94485fe9
John Rando <1168080+johnrando@users.noreply.github.com>
2026-09-13 21:09:15 -0800

    bump file version

--- src/InventoryTweaks/Properties/AssemblyInfo.cs
+++ src/InventoryTweaks/Properties/AssemblyInfo.cs
@@ -4,6 +4,6 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("InventoryTweaks")]
 [assembly: AssemblyDescription("Inventory quality-of-life for Undead Legacy: sort scrolls to top, lockable sort order, new-item highlight.")]
 [assembly: AssemblyProduct("InventoryTweaks")]
-[assembly: AssemblyVersion("0.0.0.1")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("0.0.0.2")]
+[assembly: AssemblyFileVersion("0.0.0.2")]
 [assembly: ComVisible(false)]
